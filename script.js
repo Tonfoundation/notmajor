918a8b445e108f07c5c5a97b784eac9e89a84b92
Tonfoundation <Tonfoundation2.0@proton.me>
2024-11-28 16:20:41 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const video = document.getElementById('video');
 const playButton = document.getElementById('playButton');
+const placeholder = document.getElementById('placeholder');
 
 playButton.addEventListener('click', () => {
-    if (video.paused) {
-        video.play();
-        video.muted = false; // Enable sound
-        playButton.innerHTML = '<img src="./crooked_star.png" alt="Pause Icon" class="button-icon">';
+    if (video.style.display === 'none') {
+        placeholder.style.display = 'none'; // Hide placeholder
+        video.style.display = 'block'; // Show video
+        video.muted = false; // Unmute the video
+        video.play(); // Start playing the video
     } else {
         video.pause();
-        playButton.innerHTML = '<img src="./crooked_star.png" alt="Play Icon" class="button-icon">';
+        video.style.display = 'none'; // Hide video
+        placeholder.style.display = 'flex'; // Show placeholder
     }
+});
+
+video.addEventListener('ended', () => {
+    video.style.display = 'none'; // Hide video
+    placeholder.style.display = 'flex'; // Show placeholder
 });
